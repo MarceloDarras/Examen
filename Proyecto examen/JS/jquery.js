f91fd89b0225fcaef02d13b0d7feb121a3fb4f3e
Marcelo Darras <75511290+MarceloDarras@users.noreply.github.com>
2023-05-08 19:49:00 -0400

    Add files via upload

---
+++ Proyecto examen/JS/jquery.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    
+})
+$(function(){
+    $("#formularioProducto").validate({
+        rules:{
+            producto:{
+                required: true,
+                minlength: 35
+            },
+            id:{
+                required: true,
+                minlength: 1
+            }
+        }
+    })
+})
