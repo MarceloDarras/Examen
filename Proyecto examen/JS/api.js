8a94e04d23458d077d0c38510a6ad766704565df
Marcelo Darras <75511290+MarceloDarras@users.noreply.github.com>
2023-05-10 09:58:17 -0400

    Add files via upload

--- Proyecto examen/JS/api.js
+++ Proyecto examen/JS/api.js
@@ -1,5 +1,5 @@
 var myHeaders = new Headers();
-myHeaders.append("apikey", "OkO5fwS7tOOhOGVjrtBRwEmZy6AKbl93");
+myHeaders.append("apikey", "gp9Frst3F3G0sRlTVfgTt8iiG3fWPqGx");
 
 var requestOptions = {
   method: 'GET',
@@ -7,10 +7,30 @@ var requestOptions = {
   headers: myHeaders
 };
 
-fetch("https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from}&amount={amount}", requestOptions)
+fetch("https://api.apilayer.com/exchangerates_data/convert?to={JPY}&from={USD}&amount={5}", requestOptions)
   .then(response => response.text())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
+
+
+
+  if ("geolocation" in navigator) {
+    /* geolocation is available */
+    console.log("Wena wena");
+  } else {
+    /* geolocation IS NOT available */
+    console.log("mala mala");
+  }
+  
+  navigator.geolocation.getCurrentPosition((position) => {
+    doSomething(position.coords.latitude, position.coords.longitude);
+  });
+
+  const watchID = navigator.geolocation.watchPosition((position) => {
+    doSomething(position.coords.latitude, position.coords.longitude);
+  });
+  
+  
 
 
 
