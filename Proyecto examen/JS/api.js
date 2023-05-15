89fd1327c18a907c01ef4f39632da4022d846666
Marcelo Darras <75511290+MarceloDarras@users.noreply.github.com>
2023-05-15 00:01:18 -0400

    Add files via upload

--- Proyecto examen/JS/api.js
+++ Proyecto examen/JS/api.js
@@ -1,5 +1,7 @@
 var myHeaders = new Headers();
-myHeaders.append("apikey", "gp9Frst3F3G0sRlTVfgTt8iiG3fWPqGx");
+myHeaders.append("apikey", "vbix3Le0zOibhcasnMxDgk90BoyPU46R");
+
+document.getElementsByClassName('signo').innerHTML = 'CLP'; 
 
 var requestOptions = {
   method: 'GET',
@@ -7,16 +9,41 @@ var requestOptions = {
   headers: myHeaders
 };
 
+const to = 'CLP';
+const from = 'USD';
+
+
 function cambioMoneda(){
-  if(document.getElementsByClassName('option').value = 1){
-    console.log("Divisa por defecto");
-  }else if(document.getElementsByClassName('option').value = 2){
-    fetch("https://api.apilayer.com/exchangerates_data/convert?to=USD&from=CLP&amount=15000", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+  var select = document.getElementById('divisa');
+  var option = select.value;
+  var pesos = document.getElementsByClassName('precio-item');
+  for(i = 0; i < document.getElementsByClassName('precio-item').length ; i++){
+    const amount = document.getElementsByClassName('precio-item')[i].innerHTML;
+    if(option == 1){
+      console.log("Divisa por defecto");
+      for(var i = 0; i < pesos.length; i++){
+         pesos[i].innerHTML = pesos[i].getAttribute('data-clp');
+      }
+    }else if(option == 2){
+      fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=1`, requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        var resultado = data.result;
+        console.log(resultado);
+        for(var i = 0; i < pesos.length; i++){
+          var clpValue = pesos[i].getAttribute('data-clp');
+          var usdValue = clpValue / resultado;
+          pesos[i].innerHTML = usdValue.toFixed(2);
+        }
+      })
+      .catch(error => console.log('error', error));
+    }
   }
 }
+
+
+
+
 
 
 
